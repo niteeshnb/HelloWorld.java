42b8c4255e7c4d61732778de00b2683dd2126d96
niteeshnb <86323565+niteeshnb@users.noreply.github.com>
2026-04-29 00:14:56 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, Prince and Niteesh , jay from batch-43!");
+        System.out.println("Hello, Prince and Niteesh-dev , jay from batch-43!");
     }
 }
